4d8593962785ca56ffe82a18b21c1ea6dd8a8e9a
EireHauler <winterspringtiger@gmail.com>
2021-09-20 20:10:32 -0700

    compete-version

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_concept/screens/main_screen.dart';
+import 'package:flutter_travel_concept/util/const.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: Constants.appName,
+      theme: Constants.lightTheme,
+      darkTheme: Constants.darkTheme,
+      home: MainScreen(),
+    );
+  }
+}
